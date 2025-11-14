b0edcef4d8969ff94ee89725bbb531f6c241feae
John Ranaudo <92143207+sfc-gh-jranaudo@users.noreply.github.com>
2025-11-14 12:59:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "chanzuckerberg/snowflake"
-      version = "0.25.17"
+      source = "snowflakedb/snowflake"
     }
   }
-
   backend "remote" {
     organization = "jsnow9191"
 
@@ -16,6 +14,12 @@ terraform {
 }
 
 provider "snowflake" {
+    organization_name = SFSENORTHAMERICA
+    account_name      = SFSENORTHAMERICA_JPR_DEMO
+    user              = "JP"
+    role              = "ACCOUNTADMIN"
+    authenticator     = "PROGRAMMATIC_ACCESS_TOKEN"
+    password          = "eyJraWQiOiIxNjk1MjgzMjkyMjgwODMyNiIsImFsZyI6IkVTMjU2In0.eyJwIjoiMTAxMDQ2NzU4ODoyNTg2Nzk3MDM0MjkiLCJpc3MiOiJTRjoxMDA5IiwiZXhwIjoxNzY0NDM5MDIwfQ.mcifBUA_TCbA5oh8boP60f4EYOtdPUwWzZ0LcWT9ilJcIET--2VSZfyffaX0fFkvciwDUF7iDple6pEceO5bYw"
 }
 
 resource "snowflake_database" "wtw_demo_db" {
